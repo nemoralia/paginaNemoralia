ad1ba5b8aa5c0b3bfa85e90cd3340d40362c86ef
Nemoralia <f3nriblack@gmail.com>
2025-02-24 14:00:13 -0500

    se modica login para que funcione con el api backend

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,30 +1,105 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Login.css';
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
 
 function Login() {
+  const [usuario, setUsuario] = React.useState("");
+  const [clave, setClave] = React.useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const res = await fetch(
+        `http://localhost:3000/login?usuario=${usuario}&clave=${clave}`,
+        { credentials: "include" }
+      );
+      if (res.ok) {
+        alert("Inicio de sesión correcto");
+      } else {
+        alert("Inicio de sesión incorrecto");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function validar() {
+    try {
+      const res = await fetch("http://localhost:3000/validar", {
+        credentials: "include",
+      });
+      if (res.ok) {
+        alert("Inicio de sesión correcto");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    validar();
+  }, []);
+
   return (
     <div className="container-full-height">
       <div className="w-100">
         {/* CUADRO ROJO */}
         <div className="border red-box">
-          <h2 className="text-center">¡Hola de nuevo! <br />Inicia sesión para continuar.</h2>
+          <h2 className="text-center">
+            ¡Hola de nuevo! <br />
+            Inicia sesión para continuar.
+          </h2>
         </div>
         {/* CUADRO NEGRO */}
         <div className="border shadow black-box">
-          <form className="text-center">
+          <form className="text-center" onSubmit={handleSubmit}>
             <div className="mb-3-login">
-              <label htmlFor="exampleInputEmail1" className="form-label form-label-white">Usuario<br /></label>
-              <input type="email" className="form-control input-center login-input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="INGRESA TU USUARIO" />
+              <label
+                htmlFor="exampleInputEmail1"
+                className="form-label form-label-white"
+              >
+                Usuario
+                <br />
+              </label>
+              <input
+                type="text"
+                className="form-control input-center login-input"
+                id="exampleInputEmail1"
+                placeholder="INGRESA TU USUARIO"
+                value={usuario}
+                onChange={(e) => setUsuario(e.target.value)}
+              />
             </div>
+
             <div className="mb-3-login">
-              <label htmlFor="exampleInputPassword1" className="form-label form-label-white">Contraseña<br /></label>
-              <input type="password" className="form-control input-center login-input" id="exampleInputPassword1" placeholder="INGRESA TU CONTRASEÑA" />
+              <label
+                htmlFor="exampleInputPassword1"
+                className="form-label form-label-white"
+              >
+                Contraseña
+                <br />
+              </label>
+              <input
+                type="password"
+                className="form-control input-center login-input"
+                id="exampleInputPassword1"
+                placeholder="INGRESA TU CONTRASEÑA"
+                value={clave}
+                onChange={(e) => setClave(e.target.value)}
+              />
             </div>
-            <button type="submit" className="btn btn-secondary">Iniciar sesión</button>
+
+            <button type="submit" className="btn btn-secondary">
+              Iniciar sesión
+            </button>
             <div className="mt-3 text-center">
-              <Link to="/olvidaste" className="text-link"><br></br>¿Olvidaste tu contraseña?</Link><br />
-              <Link to="/registro" className="text-link"><br></br>¿No tienes cuenta? Regístrate aquí</Link>
+              <Link to="/olvidaste" className="text-link">
+                <br></br>¿Olvidaste tu contraseña?
+              </Link>
+              <br />
+              <Link to="/registro" className="text-link">
+                <br></br>¿No tienes cuenta? Regístrate aquí
+              </Link>
             </div>
           </form>
         </div>
