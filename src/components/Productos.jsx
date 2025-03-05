6ca6e862e937a4c0b16324b5a4465060893deb73
Nemoralia <f3nriblack@gmail.com>
2025-03-05 14:26:51 -0500

    "Se conecta productos a la base de datos, pendiente CRUD"

--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './Productos.css';
 
 function Productos() {
+  const [productos, setProductos] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/productos')
+      .then(response => {
+        setProductos(response.data);
+      })
+      .catch(error => {
+        console.error('Error al obtener los productos:', error);
+      });
+  }, []);
+
   return (
     <div>
       <h1>Productos</h1>
-      <p>Lista de productos...</p>
+      <br></br>
+      <div className="productos-grid">
+        {productos.map((producto) => (
+          <div key={producto.ID} className="producto-item">
+            <img src={producto.imagen} alt={producto.nombre} />
+            <h2>{producto.nombre}</h2>
+            <p>Precio: {producto.precio} COP</p>
+            <p>Características: {producto.caracteristicas}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
